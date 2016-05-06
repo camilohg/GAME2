a5e93aad09b49db728123882fee8591d461cb839
camilohg <felipeluquebello@MacBook-Pro-de-Felipe-3.local>
2016-05-05 23:32:45 -0500

    luquee

---
+++ terrain/Assets/Destroy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+	public String Event;
+
+	public String CollisionTag;
+	public bool AllowFadeout = true;
+	public bool TriggerOnce = false;
+
+
+	private bool hasTriggered;
+	private bool isQuitting;
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.K)) {
+			Destroy (gameObject);
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (String.IsNullOrEmpty(CollisionTag) || other.CompareTag(CollisionTag))
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+}
