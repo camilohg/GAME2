370a16dbc315f23f11ab217c709645d6ab720534
bonillasaavedra <mikebonillav@outlook.com>
2016-05-06 14:55:29 -0500

    puntaje y vida

---
+++ terrain/Assets/scritps/moneda.js
@@ -0,0 +1,19 @@
+﻿// JavaScript
+var score = 0;
+
+function OnTriggerEnter( other : Collider ) {
+    Debug.Log("OnTriggerEnter() was called");
+    if (other.tag == "Appletag") 
+    {
+        Debug.Log("Other object is a apple");
+        score += 5;
+        Debug.Log("Score is now " + score);
+        Destroy(other.gameObject);
+    }
+}
+
+function OnGUI() 
+{
+	GUILayout.Label( "Puntos = " + score );
+
+}
